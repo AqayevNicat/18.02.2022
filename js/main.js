b4dfa9799b1eb982f60eba32f36f4edae6c98c8d
AqayevNicat <nijatam@code.edu.az>
2022-02-17 22:33:54 +0400

    18.02.2022

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Task 1
+var myBody = document.getElementById("body");
+if(confirm()){
+    myBody.style.backgroundColor = "black";
+}
+
+
+// Task 2
+// if(prompt() > 18){
+//     alert("Daxil oldunuz ...");
+// }
+// else{
+//     alert("Duzgun daxil edin ...")
+// }
+
+
+// Task 3
+// class Program{
+//     constructor(name,yas){
+//         this.name = name;
+//         this.yas = yas;
+//     }
+// }
+// var h1 = new Program("Nicat",19);
+
+// const Human = {
+//     name : "Nicat",
+//     surname : "Agayev",
+//     password : "nicat2002",
+//     get fullname(){
+//         return `${this.name} ${this.surname}`;
+//     },
+//     Upper(){
+//         document.write(this.fullname.toUpperCase());
+//     }
+// }
+
+// console.log(Human);
